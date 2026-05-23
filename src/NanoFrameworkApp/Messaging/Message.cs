36b453b02d06a642d0784ac8c19df9680374b15f
Jeffrey Palermo <jeffrey@palermo.com>
2026-05-22 23:35:12 -0500

    Broken build to prove PR merge is blocked

--- src/NanoFrameworkApp/Messaging/Message.cs
+++ src/NanoFrameworkApp/Messaging/Message.cs
@@ -2,7 +2,7 @@ namespace NanoFrameworkApp.Messaging
 {
     public class Message
     {
-        public string Topic { get; }
+        public string Topic SYNTAX_ERROR { get; }
         public string Payload { get; }
 
         public Message(string topic, string payload)
